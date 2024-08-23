ffe54a9afd78062d8c2a428d65a75a3e853bb268
Majed Alshehri <143289764+majedalshehri1@users.noreply.github.com>
2024-08-24 00:10:03 +0300

    Add content to file TodosForm

---
+++ src/components/todos/TodosForm.svelte
@@ -0,0 +1,10 @@
+<!-- Todos Structure -->
+<div class="todos-form">
+  <div class="todos-form_icon">icon</div>
+  <div class="todos-form_form">
+    <input type="text" placeholder="أضف مهمة جديدة ..." />
+  </div>
+  <div class="todos-form_submit">
+    <button class="btn"> اضافة </button>
+  </div>
+</div>
