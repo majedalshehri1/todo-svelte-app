b430122a44fa5b73bf6ea606e7bd6f2b969ecc3b
Majed Alshehri <143289764+majedalshehri1@users.noreply.github.com>
2024-08-23 23:49:23 +0300

    Add Header component and call it in App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<!-- Import components to use it -->
+ <script>
+  import TodoPages from "./pages/TodoPages.svelte";
+  import Header from "./components/layout/Header.svelte";
+ </script>
+
+
+ <div class="App">
+  <TodoPages />
+  <Header />
+ </div>
