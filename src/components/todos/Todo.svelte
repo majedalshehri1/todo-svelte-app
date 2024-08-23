4d2c0d61b64cc366f609d741d09a1badd5783141
Majed Alshehri <143289764+majedalshehri1@users.noreply.github.com>
2024-08-24 00:18:19 +0300

    Import Todo in list (Todos.svelte file)

---
+++ src/components/todos/Todo.svelte
@@ -0,0 +1,9 @@
+<div class="todos-todo">
+  <!-- 3 type into the div todo -->
+   <div class="todos-todo_icon">icon</div>
+   <div class="todos-todo_text">text</div>
+   <div class="todos-todo_cta">
+    <div class="todos-todo_cta-edit">icon</div>
+    <div class="todos-todo_cta-delete">icon</div>
+   </div>
+</div>
---
+++ src/components/todos/Todos.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Todo from "./Todo.svelte";
+</script>
+
+<div class="todos-list">
+  <Todo />
+  <Todo />
+  <Todo />
+  <Todo />
+  <Todo />
+  <Todo />
+  <Todo />
+  <Todo />
+</div>
