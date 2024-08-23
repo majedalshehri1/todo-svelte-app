73677cf1e8b03803cd5a717b142d74757e7f0776
Majed Alshehri <143289764+majedalshehri1@users.noreply.github.com>
2024-08-24 00:24:18 +0300

    Install svelte-feather-icons library with import it

--- src/components/todos/TodosForm.svelte
+++ src/components/todos/TodosForm.svelte
@@ -1,6 +1,12 @@
-<!-- Todos Structure -->
+<script>
+  import { CircleIcon } from "svelte-feather-icons";
+</script>
+
+<!-- TodosFrom Structure -->
 <div class="todos-form">
-  <div class="todos-form_icon">icon</div>
+  <div class="todos-form_icon">
+    <CircleIcon />
+  </div>
   <div class="todos-form_form">
     <input type="text" placeholder="أضف مهمة جديدة ..." />
   </div>
